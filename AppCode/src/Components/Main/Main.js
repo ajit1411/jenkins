b6e8e3f7da40f8ad04fed19b23c444fa23869629
ajitjadhav2310@outlook.com <ajitjadhav2310@outlook.com>
2021-01-14 21:00:53 +0530

    title changed

--- AppCode/src/Components/Main/Main.js
+++ AppCode/src/Components/Main/Main.js
@@ -75,7 +75,7 @@ const Main = () => {
                         isLoggedIn ? (
                             <React.Fragment>
                                 <span className='title'>
-                                    To Do App
+                                    To Do App - Ajit
                                 </span>
                                 <div className='main__user-form'>
                                     <div className='input'>
